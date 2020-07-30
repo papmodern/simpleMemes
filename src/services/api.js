320ceea85f36ada3dd961ad17aaa68f9dfe6546f
Hamed Hamedi <papmodern14@gmail.com>
2020-07-30 14:11:07 +0430

    Make github pages available

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,20 @@
 import axios from 'axios'
 
 export default class Api {
-    static api = axios.create();
+    static api = axios.create({
+        baseURL: 'https://api.imgur.com/3',
+        headers: {
+            'Authorization': 'Client-ID b7a38f5989011ba'
+        }
+    });
 
     static async getImages({section, sort, window, page, showViral}) {
-        const res = await axios.get(`/api/gallery/${section}/${sort}/${window}/${page}?showViral=${showViral}`);
+        const res = await Api.api.get(`/gallery/${section}/${sort}/${window}/${page}?showViral=${showViral}`);
         return res.data.data;
     }
 
     static async getAlbum(id) {
-        const res = await axios.get(`/api/gallery/album/${id}`);
+        const res = await Api.api.get(`/gallery/album/${id}`);
         return res.data.data;
     }
 }
