a9f58df11f02fa73eb83e9bd6d936e66bb9eb044
Hamed Hamedi <papmodern14@gmail.com>
2020-07-30 14:06:19 +0430

    Updates

---
+++ precache-manifest.9b86cf753c7f1cd1615bed2c46763419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5361881302e353740fb4bdb91b675f4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3777169fb63ce6207f",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "4918e826e647e4dc9658",
+    "url": "/static/js/2.3e0143d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e0143d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3777169fb63ce6207f",
+    "url": "/static/js/main.5e7b4f80.chunk.js"
+  },
+  {
+    "revision": "3bf51bc879bb131d88a9",
+    "url": "/static/js/runtime-main.d8b72d64.js"
+  }
+]);
